65397a0c859e8d614b441739296811971b88ea26
Ricardo Rivero <ricardo@enzadon.com>
2025-07-12 20:32:50 -0400

    add agent loop

---
+++ src/ai/agent/browser-agent/agentLoop.ts
@@ -0,0 +1,40 @@
+import {
+  ChatCompletionMessage,
+  ChatCompletionMessageParam,
+} from "openai/resources";
+import { callLlm } from "../../providers/openRouter";
+import { TestTools } from "../tools";
+import { executeWebDriverLoop } from "./webDriverLoop";
+import { GeminiModel, LLMModel } from "../../providers/models";
+
+export const callLlmAgentLoop = async (
+  contents: ChatCompletionMessageParam[],
+  driver: WebdriverIO.Browser,
+  model: LLMModel = GeminiModel.GEMINI_2_5_PRO
+): Promise<ChatCompletionMessage> => {
+  console.log("[ callLlmAgentLoop ] with", contents.length, "contents");
+  const actionSteps: string[] = [];
+
+  const response = await callLlm(contents, model, TestTools);
+
+  const toolCalls = response.tool_calls;
+
+  if (toolCalls && toolCalls.length > 0) {
+    console.log(
+      "[ callLlmAgentLoop ] with",
+      toolCalls.length,
+      "function calls"
+    );
+    const newContents = await executeWebDriverLoop(
+      toolCalls,
+      contents,
+      driver,
+      actionSteps
+    );
+
+    console.log("[ callLlmAgentLoop ] calls executed, recursing");
+    return await callLlmAgentLoop(newContents, driver, model);
+  }
+  console.log("[ callLlmAgentLoop ] no function calls, returning");
+  return response;
+};
