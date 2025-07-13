1382f9cfd8dccb1ce09fa71bc84f9dc8945a4abe
Ricardo Rivero <ricardo@enzadon.com>
2025-07-12 20:22:55 -0400

    ignore dist

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,6 +5,9 @@ import { defineConfig } from "eslint/config";
 
 
 export default defineConfig([
+  {
+    ignores: ["dist/**/*"]
+  },
   { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], plugins: { js }, extends: ["js/recommended"] },
   { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], languageOptions: { globals: globals.browser } },
   tseslint.configs.recommended,
