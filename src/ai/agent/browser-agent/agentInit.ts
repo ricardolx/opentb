72b223ef0377afc99ecd6f7f0168c4ec54848db6
Ricardo Rivero <ricardo@enzadon.com>
2025-07-12 20:32:55 -0400

    add agent init

---
+++ src/ai/agent/browser-agent/agentInit.ts
@@ -0,0 +1,49 @@
+import { callLlmAgentLoop } from "./";
+import { GeminiModel, LLMModel } from "../../providers/models";
+import { ChatCompletionMessageParam } from "openai/resources";
+
+export const callAgentInit = async (
+  prompt: string,
+  systemPrompt: string,
+  driver: WebdriverIO.Browser,
+  model: LLMModel = GeminiModel.GEMINI_2_5_PRO
+) => {
+  console.log("[ callAgentInit ] with", prompt.substring(0, 30), "...");
+
+  const content: ChatCompletionMessageParam[] = [
+    {
+      role: "user",
+      content: prompt,
+    },
+  ];
+
+  const pageSource = await driver.getPageSource();
+  const base64Screenshot = await driver.saveScreenshot(`screen-first.png`);
+  const base64PageSource = base64Screenshot.toString("base64");
+
+  content.push({
+    role: "system",
+    content: systemPrompt,
+  });
+
+  content.push({
+    role: "user",
+    content: `Here is the page source: ${pageSource}`,
+  });
+  content.push({
+    role: "user",
+    content: base64PageSource,
+  });
+
+  const time = Date.now();
+  const response = await callLlmAgentLoop(content, driver, model);
+  const duration = Date.now() - time;
+
+  console.log("[ callLlmAgentLoop ] ✅ complete");
+  console.log(
+    "[ Duration ] 🕒 ",
+    duration > 60000 ? `${duration / 60000}m` : `${duration}ms`
+  );
+
+  return response;
+};
